a9ab2c869a9730b8ab77b9404e94554414015541
Magnus Ahlberg <magnus.ahlberg@swegon.com>
2020-11-13 11:13:39 +0100

    Added minimal repro case for creating migration scripot failure

---
+++ EF.Core.Failure/FailureContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.IO;
+
+namespace EF.Core.Failure
+{
+    public class FailureContext : DbContext
+    {
+        public FailureContext(ILogger<FailureContext> logger)
+        {
+
+        }
+
+        public FailureContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var appsettingsJson = Path.Combine(currentDirectory, "appsettings.json");
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .SetBasePath(currentDirectory);
+            configurationBuilder.AddJsonFile(appsettingsJson);
+            IConfiguration configuration = configurationBuilder.Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("FailureDb"));
+            optionsBuilder.UseLazyLoadingProxies();
+        }
+
+        DbSet<Item> Items { get; set; }
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+    }
+
+}
